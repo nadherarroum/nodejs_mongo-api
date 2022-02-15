ff914ff5e6ece674c5ba2e2561382eb7356f8e91
Nadher Arroum <nadher.arroum@gmail.com>
2022-02-15 10:26:51 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
+## Rest API with NodeJs + MongoDB
+
+### Installation
+
+```bash
 > npm init
 > npm i nodemon
 > npm install mongoose
 > npm i debug
 > npm i config
+```
 
-set mySecret=Password
+## Environment variable
 
+> set mySecret=Password
+
+## Install Express and run the project with npm
+
+```bash
 > npm i express
-
 > npm start
+```
--- routers/courses.js
+++ routers/courses.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const {Course} = require('../models/course');
 
-
+// Display all courses
 router.get('', async (req,res)=>{
     let courses = await Course.find();
     res.send(courses);
 });
 
+// ADD new course
 router.post('', async (req,res)=>{
     let course = new Course(req.body);
     course = await course.save();
